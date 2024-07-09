fed5f345a2d85d109ad5f06334f62b48b49ff1a6
vijay711-github <vj7112000@gmail.com>
2024-07-09 23:45:28 +0530

    Middleware in the Hono_app

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,14 +1,24 @@
-import { Hono } from 'hono'
+import { Hono, Next } from 'hono'
+import { Context } from 'hono/jsx';
 
 const app = new Hono()
 
+app.use(async (c, next) => {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  } else {
+    return c.text("You dont have acces");
+  }
+})
+
 app.get('/', async (c) => {
-  const body = await c.req.json()
+  const body = await c.req.parseBody()
   console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
 
-  return c.text('Hello Hono!')
+  return c.json({msg: "as"})
 })
 
 export default app
